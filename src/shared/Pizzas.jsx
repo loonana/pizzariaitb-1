f708475250fff19f0ab0fde4f704c3819f7b0f7d
loonana <itsloye@gmail.com>
2024-08-06 11:59:44 -0300

    imagens

--- src/shared/Pizzas.jsx
+++ src/shared/Pizzas.jsx
@@ -1,8 +1,8 @@
-import mussarela from "../assets/images/mussarela.png"
-import calabresa from "../assets/images/calabresa.png"
-import atum from "../assets/images/atum.png"
-import portuguesa from "../assets/images/portuguesa.png"
-import frango from "../assets/images/frango.png"
+import mussarela from "../assets/images/pizza-mussarela.jpeg"
+import calabresa from "../assets/images/pizzacalabresa.jfif"
+import atum from "../assets/images/pizza-atum.webp"
+import portuguesa from "../assets/images/pizza-portuguesa.jpeg"
+import frango from "../assets/images/pizza-frango.jfif"
 
 const Pizzas = [
     {
